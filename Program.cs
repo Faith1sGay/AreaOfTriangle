f8b94c66523b6ed8723e8353ac235518249cd708
Faith1sGay <49874713+Faith1sGay@users.noreply.github.com>
2020-10-31 13:49:20 -0400

    this.height = height;

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AreaOfTriangle
+
+{
+    class Triangle
+    {
+        public int height, width;
+        public Triangle(int height, int width)
+        {
+            this.height = height;
+            this.width = width;
+        }
+    }
+    class FindArea
+    {
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                Triangle tri1 = new Triangle(12, 3);
+                Console.WriteLine($"The area of the triangle is {tri1.height * tri1.width / 2}.");
+            }
+        }
+    }
+}
